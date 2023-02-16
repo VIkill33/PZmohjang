890acc6b944c49cf27196cf91d521b5bbfb0a271
Vikill <vikill1024@163.com>
2023-02-16 20:13:28 +0800

    Initial Commit

---
+++ PZmohjang/PZmohjangApp.swift
@@ -0,0 +1,17 @@
+//
+//  PZmohjangApp.swift
+//  PZmohjang
+//
+//  Created by Vikill Blacks on 2023/2/16.
+//
+
+import SwiftUI
+
+@main
+struct PZmohjangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
